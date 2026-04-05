6c66770a892e5ab56a98f1a5bc1f465135b325e6
Saristeidyn <129422890+Saristeidyn@users.noreply.github.com>
2026-04-04 20:13:52 -0600

    made the bird go up, still need to fix how boring it is though

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.linearVelocity = Vector2.up * 10;
+        }
+    }
+}
