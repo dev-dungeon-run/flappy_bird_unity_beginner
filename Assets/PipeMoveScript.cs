01c10bcdc35a7976bad1d8e2f0d82b5c66a24e00
Saristeidyn <129422890+Saristeidyn@users.noreply.github.com>
2026-04-06 19:20:20 -0600

    Now keeps score

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PipeMoveScript : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 5;
+    public float deadZone = -45;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,11 @@ public class PipeMoveScript : MonoBehaviour
     void Update()
     {
         transform.position = transform.position +(Vector3.left * moveSpeed * Time.deltaTime);
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Destroyed");
+            Destroy(gameObject);
+        }
         
     }
 }
